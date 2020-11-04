814529b3a08f076f81e9bb71790e2c331e929fa1
Jono Cooper <jono@jonocooper.com>
2020-11-04 22:34:47 +1300

    Disable local config

--- deployment/provider.tf
+++ deployment/provider.tf
@@ -3,8 +3,9 @@ data "digitalocean_kubernetes_cluster" "sfo2" {
 }
 
 provider "kubernetes" {
-  host  = data.digitalocean_kubernetes_cluster.sfo2.endpoint
-  token = data.digitalocean_kubernetes_cluster.sfo2.kube_config[0].token
+  load_config_file = false
+  host             = data.digitalocean_kubernetes_cluster.sfo2.endpoint
+  token            = data.digitalocean_kubernetes_cluster.sfo2.kube_config[0].token
   cluster_ca_certificate = base64decode(
     data.digitalocean_kubernetes_cluster.sfo2.kube_config[0].cluster_ca_certificate
   )
