5d0754ac8f502d02dbd5813a50f6c9413dad8bf5
Jono Cooper <jono@jonocooper.com>
2021-04-14 21:32:40 +1200

    Update provider

--- deployment/provider.tf
+++ deployment/provider.tf
@@ -3,9 +3,9 @@ data "digitalocean_kubernetes_cluster" "sfo2" {
 }
 
 provider "kubernetes" {
-  load_config_file = false
-  host             = data.digitalocean_kubernetes_cluster.sfo2.endpoint
-  token            = data.digitalocean_kubernetes_cluster.sfo2.kube_config[0].token
+  version = "= 1.10.0"
+  host    = data.digitalocean_kubernetes_cluster.sfo2.endpoint
+  token   = data.digitalocean_kubernetes_cluster.sfo2.kube_config[0].token
   cluster_ca_certificate = base64decode(
     data.digitalocean_kubernetes_cluster.sfo2.kube_config[0].cluster_ca_certificate
   )
